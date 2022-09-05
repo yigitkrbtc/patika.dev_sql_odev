50649c109d38533d2e0e150ad4c3b759f65510fd
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2022-09-05 16:50:11 +0300

    Create odev7.sql

---
+++ odev7.sql
@@ -0,0 +1,14 @@
+1)SELECT rating,COUNT(*) FROM film
+Group BY rating;
+
+2)SELECT replacement_cost,COUNT(*) FROM film
+GROUP BY replacement_cost
+HAVING COUNT(replacement_cost)>50;
+
+3)SELECT store_id,COUNT(*) FROM customer
+GROUP BY store_id
+
+4) SELECT country_id, COUNT(*) FROM city
+GROUP BY country_id
+ORDER BY COUNT(*) DESC
+LIMIT 1;
