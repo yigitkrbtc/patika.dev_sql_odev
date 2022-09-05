76be4d239d894f63df04fccbb6ee941b53320981
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2022-09-05 16:50:24 +0300

    Update odev7.sql

--- odev7.sql
+++ odev7.sql
@@ -6,7 +6,7 @@ GROUP BY replacement_cost
 HAVING COUNT(replacement_cost)>50;
 
 3)SELECT store_id,COUNT(*) FROM customer
-GROUP BY store_id
+GROUP BY store_id;
 
 4) SELECT country_id, COUNT(*) FROM city
 GROUP BY country_id
