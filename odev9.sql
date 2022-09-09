c50300f7752fe83b06a4f1ee9fcb7847d99d6673
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2022-09-09 15:13:57 +0300

    Create odev9.sql

---
+++ odev9.sql
@@ -0,0 +1,8 @@
+SELECT city,country FROM city
+INNER JOIN country ON country.country_id = city.country_id;
+
+SELECT payment_id,first_name,last_name FROM customer
+INNER JOIN payment ON payment.customer_id = customer.customer_id
+
+SELECT rental_id,first_name,last_name FROM customer
+INNER JOIN rental ON rental.customer_id = customer.customer_id
